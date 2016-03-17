b528f7db473f8d0c7dfafd4620f172b24e18c1c4
ShreyasHemachandran <shreyas.hemachandran@gmail.com>
2016-03-17 17:11:25 +0530

    Changes regarding FishShell

---
+++ my_setup/dotfiles/shreyas_fish/aliases.fish
@@ -0,0 +1,42 @@
+# some more ls aliases
+alias ll 'ls -alF'
+alias la 'ls -A'
+alias l 'ls -CF'
+alias sl 'ls'
+
+# Work Space Shortcut
+alias git_repo 'cd ~/Dropbox/git_repo/'
+alias l_repo 'cd ~/My_Passport/git_ripo'
+alias nsi 'cd /home/shreyas/My_Passport/nsi_vpu/'
+alias otg 'cd /home/shreyas/My_Passport/otg/'
+alias sshlpc 'ssh -X allgo@172.16.0.161'
+alias git_off 'cd ~/Dropbox/git_off/'
+alias lpc 'cd /mnt/labpc/'
+
+# App Shorcuts
+alias gi 'gvim'
+alias v 'vim'
+alias xt 'xterm -fa 'Monospace' -fs 9 -maximized &'
+
+# pushd and popd aliases
+alias Pd 'pushd'
+alias pd 'popd'
+
+# cd aliases
+alias .. 'cd ..'
+alias cd.. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../../'
+alias ..2 'cd ../..'
+alias ..3 'cd ../../..'
+alias ..4 'cd ../../../../'
+
+# Git Aliases
+alias G 'git'
+alias Gs 'git status'
+alias Gba 'git branch -a'
+alias Gl 'git log'
+
+# GCC with all warnings
+alias Gcc 'gcc -Wall -Werror -w -Wextra'
---
+++ my_setup/dotfiles/shreyas_fish/config.fish
@@ -0,0 +1,7 @@
+# Aliases Are Written in this file
+. ~/.config/fish/aliases.fish
+
+# Exporting Editor as vim
+set -x EDITOR=vim
+
+
