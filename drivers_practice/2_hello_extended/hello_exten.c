86831581ab92098446b2631fa006304e776aa0e6
ShreyasHemachandran <shreyas.hemachandran@gmail.com>
2018-08-22 13:04:35 +0530

    Driver Practice

---
+++ drivers_practice/2_hello_extended/hello_exten.c
@@ -0,0 +1,17 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+static int __init driver_init(void){
+	printk(KERN_ALERT "Hello Driver");
+	return 0;
+}
+
+static void __exit driver_exit(void){
+	printk(KERN_ALERT "Bye Bye Driver");
+}
+
+module_init(driver_init);
+module_exit(driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shreyas");
