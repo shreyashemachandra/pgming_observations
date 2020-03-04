99b910b95bd1f9feb39dd640ff58c38f9b58e5d0
Shreyas Hemachandran <shreyas@allgosystems.com>
2020-03-04 18:39:23 +0530

    CMAKE Cross compilation

---
+++ cmake/helloworld/bin/CMakeFiles/hello.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.elf.dir/src/main.c.o"
+  "CMakeFiles/hello.elf.dir/src/startup.S.o"
+  "CMakeFiles/hello.elf.dir/src/system_MIMX8MQ6_cm4.c.o"
+  "hello.elf"
+  "hello.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/hello.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
